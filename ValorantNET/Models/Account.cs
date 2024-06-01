4af08c9797742f10882a28bd2fe319625b4e9ec0
unknown <shawnbasnett@gmail.com>
2024-06-01 18:23:22 -0400

    Added more endpoints and Rewrote Match Model

--- ValorantNET/Models/Account.cs
+++ ValorantNET/Models/Account.cs
@@ -13,6 +13,19 @@ namespace ValorantNET.Models
             public string puuid { get; set; }
             public string region { get; set; }
             public string account_level { get; set; }
+            public string name { get; set; }
+            public string tag { get; set; }
+            public Card card { get; set; }
+            public string last_update { get; set; }
+            public string last_update_raw { get; set; }
+        }
+
+        public class Card
+        {
+            public string small { get; set; }
+            public string large { get; set; }
+            public string wide { get; set; }
+            public string id { get; set; }
         }
     }
 }
--- ValorantNET/Models/Match.cs
+++ ValorantNET/Models/Match.cs
@@ -6,54 +6,111 @@ namespace ValorantNET.Models
 {
     public class Match : BaseResponse
     {
-        public string user { get; set; }
-        public List<MatchInfo> matches { get; set; }
+        public Data[] data { get; set; }
 
-        public class MatchInfo
+        public class Data
         {
-            public bool isAvailable { get; set; }
             public Metadata metadata { get; set; }
-            public Game game { get; set; }
+            public PlayerData players { get; set; }
         }
 
         public class Metadata
         {
-            public string gameid { get; set; }
-            public string modename { get; set; }
-            public bool playerhaswon { get; set; }
-            public DateTime timestamp { get; set; }
             public string map { get; set; }
-            public string agentplayed { get; set; }
+            public string game_version { get; set; }
+            public string game_length { get; set; }
+            public string game_start { get; set; }
+            public string game_start_patched { get; set; }
+            public string rounds_played { get; set; }
+            public string mode { get; set; }
+            public string mode_id { get; set; }
+            public string queue { get; set; }
+            public string season_id { get; set; }
+            public string platform { get; set; }
+            public string matchid { get; set; }
+            public PremierInfo premier_info { get; set; }
+            public string region { get; set; }
+            public string cluster { get; set; }
         }
-
-        public class Game
+        public class PremierInfo
         {
-            public int roundsplayed { get; set; }
-            public int roundswon { get; set; }
-            public int roundslost { get; set; }
-            public Kda kda { get; set; }
-            public int headshots { get; set; }
-            public string headshotspercentage { get; set; }
-            public int damagemade { get; set; }
-            public int damagereceived { get; set; }
-            public int econrating { get; set; }
-            public Playtime playtime { get; set; }
+            public string tournament_id { get; set; }
+            public string matchup_id { get; set; }
         }
-
-        public class Kda
+        public class PlayerData
         {
-            public string kda { get; set; }
-            public string kd { get; set; }
-            public int kills { get; set; }
-            public int deaths { get; set; }
-            public int assists { get; set; }
+            public Players[] all_players { get; set; }
         }
-
-        public class Playtime
+        public class Players
         {
-            public int value { get; set; }
-            public string patched { get; set; }
+            public string puuid { get; set; }
+            public string name { get; set; }
+            public string tag { get; set; }
+            public string team { get; set; }
+            public string level { get; set; }
+            public string character { get; set; }
+            public string currenttier { get; set; }
+            public string currenttier_patched { get; set; }
+            public string player_card { get; set; }
+            public string player_title { get; set; }
+            public string party_id { get; set; }
+            public SessionPlaytime session_playtime { get; set; }
+            public Assets assets { get; set; }
+            public Behavior behaviour { get; set; }
+            public AbilityCasts ability_casts { get; set; }
+            public Stats stats { get; set; }
         }
-
+        public class SessionPlaytime
+        {
+            public string minutes { get; set; }
+            public string seconds { get; set; }
+            public string milliseconds { get; set; }
+        }
+        public class Assets
+        {
+            public Card card { get; set; }
+            public Agent agent { get; set; }
+        }
+        public class Card
+        {
+            public string small { get; set; }
+            public string large { get; set; }
+            public string wide { get; set; }
+        }
+        public class Agent
+        {
+            public string small { get; set; }
+            public string full { get; set; }
+            public string bust { get; set; }
+            public string killfeed { get; set; }
+        }
+        public class Behavior
+        {
+            public string afk_rounds { get; set; }
+            public FriendlyFireData friendly_fire { get; set; }
+            public string rounds_in_spawn { get; set; }
+        }
+        public class FriendlyFireData
+        {
+            public string incoming { get; set; }
+            public string outgoing { get; set; }
+        }
+        public class AbilityCasts
+        {
+            public string c_cast { get; set; }
+            public string q_cast { get; set; }
+            public string e_cast { get; set; }
+            public string x_cast { get; set; }
+        }
+        public class Stats
+        {
+            public string score { get; set; }
+            public string kills { get; set; }
+            public string deaths { get; set; }
+            public string assists { get; set; }
+            public string bodyshots { get; set; }
+            public string headshots { get; set; }
+            public string legshots { get; set; }
+        }
     }
 }
